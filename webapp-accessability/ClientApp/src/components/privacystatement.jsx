785e8b01623790c94101654f482e901c3894341e
MaikeruB <maikelbazuin@gmail.com>
2023-12-18 12:03:47 +0100

    privacystatement pagina
    begin pagina met approute er heen

---
+++ webapp-accessability/ClientApp/src/components/privacystatement.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Privacystatement extends Component {
+    static displayName = Privacystatement.name;
+
+  render() {
+    return (
+      <div>
+        <h1>Test </h1>
+      </div>
+    );
+  }
+}
