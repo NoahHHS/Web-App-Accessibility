19047a9d07e96fc61acb9e1bfc939284c1a0e268
MaikeruB <maikelbazuin@gmail.com>
2023-12-18 13:27:37 +0100

    update privacystatement

--- webapp-accessability/ClientApp/src/components/privacystatement.jsx
+++ webapp-accessability/ClientApp/src/components/privacystatement.jsx
@@ -6,7 +6,33 @@ export class Privacystatement extends Component {
   render() {
     return (
       <div>
-        <h1>Test </h1>
+        <h1>Access-Ability</h1>
+        <section>
+            <h2>Persoonsgegevens die wij verwerken</h2>
+            <p class="beschrijving">Accessibility verwerkt uw persoonsgegevens doordat u gebruik maakt van onze diensten en/of omdat u deze zelf aan ons verstrekt. Hieronder vindt u een overzicht van de persoonsgegevens die wij verwerken: </p>
+            <p class="item">Voor- en achternaam</p>
+            <p class="item">Geslacht </p>        
+            <p class="item">Geboortedatum </p>        
+            <p class="item">Adresgegevens </p>        
+            <p class="item">Telefoonnummer </p>        
+            <p class="item">E-mailadres </p>        
+            <p class="item">IP-adres </p>      
+            <p class="item">Overige persoonsgegevens die u actief verstrekt bijvoorbeeld door een profiel op deze website aan te maken, in correspondentie en telefonisch </p>   
+            <p class="item">Locatiegegevens </p>        
+            <p class="item">Gegevens over uw activiteiten op onze website </p>        
+            <p class="item">Bankrekeningnummer </p>
+        </section>
+        <section>
+            <h2>Bijzondere en/ of gevoelige persoonsgegevens die wij verwerken </h2>
+            <p class="beschrijving">Accessibility verwerkt de volgende bijzondere en/of gevoelige persoonsgegevens van u: </p>
+            <p class="item">gezondheid </p>        
+            <p class="item">gegevens van personen jonger dan 16 jaar Onze website en/of dienst heeft niet de intentie gegevens te verzamelen over websitebezoekers die jonger zijn dan16 jaar. Tenzij ze toestemming hebben van ouders of voogd. We kunnen echter niet controleren of een bezoeker ouder dan 16 is. Wij raden ouders dan ook aan betrokken te zijn bij de online activiteiten van hun kinderen, om zo te voorkomen dat er gegevens over kinderen verzameld worden zonder ouderlijke toestemming. Als u er van overtuigd bent dat wij zonder die toestemming persoonlijke gegevens hebben verzameld over een minderjarige, neem dan contact met ons op via accessibility@bartimeus.nl, dan verwijderen wij deze informatie. </p>        
+            <p class="item">Genetische gegevens </p>        
+        </section>
+        <section>
+            <h2>Bijzondere en/of gevoelige persoonsgegevens die wij verwerken </h2>
+            <p class="beschrijving">Onze website en/of dienst heeft niet de intentie gegevens te verzamelen over websitebezoekers die jonger zijn dan 16 jaar. Tenzij ze toestemming hebben van ouders of voogd. We kunnen echter niet controleren of een bezoeker ouder dan 16 is. Wij raden ouders dan ook aan betrokken te zijn bij de online activiteiten van hun kinderen, om zo te voorkomen dat er gegevens over kinderen verzameld worden zonder ouderlijke toestemming. Als u er van overtuigd bent dat wij zonder die toestemming persoonlijke gegevens hebben verzameld over een minderjarige, neem dan contact met ons op via accessibility@bartimeus.nl, dan verwijderen wij deze informatie. </p>
+        </section>
       </div>
     );
   }
