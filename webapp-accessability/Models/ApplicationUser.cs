0eaa0451c138ce19e606df246489e70d27606f8f
Msa-786 <94054037+Msa-786@users.noreply.github.com>
2023-12-18 10:23:56 +0100

    base models gemaakt

--- webapp-accessability/Models/ApplicationUser.cs
+++ webapp-accessability/Models/ApplicationUser.cs
@@ -4,4 +4,15 @@ namespace webapp_accessability.Models;
 
 public class ApplicationUser : IdentityUser
 {
+    public string? Email;
+    public string? Wachtwoord;
+    public string? Naam;
+    public int? Telnr;
+    public string? Token;
+    public string? Functie;
+    public string? Bedrijfsnaam;
+    public int? bedrijfsTelNr;
+    public string? VoorkeurBenadering;
+    public int? AangemeldPromos;
+    public string? Beschikbaarheid;
 }
