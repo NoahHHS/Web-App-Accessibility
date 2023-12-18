84d258fff18ee984fc6e4f021d02246ebf67a325
Msa-786 <94054037+Msa-786@users.noreply.github.com>
2023-12-18 12:38:19 +0100

    FK aan Medischegegevens toegevoegd

--- webapp-accessability/Models/MedischeGevens.cs
+++ webapp-accessability/Models/MedischeGevens.cs
@@ -3,4 +3,7 @@ namespace webapp_accessability.Models;
 public class MedischeGegevens {
     public string Beperking {get; set;}
     public string? Hulpmiddelen {get; set;}
+
+    //FK
+    public string? UserID {get; set;}
 }
