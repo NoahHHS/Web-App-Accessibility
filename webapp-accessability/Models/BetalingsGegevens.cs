296a17cf506000c384c4f5280eadf13150bf30a9
NoahHHS <101333810+NoahHHS@users.noreply.github.com>
2023-12-18 11:34:01 +0100

    Update BetalingsGegevens.cs

--- webapp-accessability/Models/BetalingsGegevens.cs
+++ webapp-accessability/Models/BetalingsGegevens.cs
@@ -1,6 +1,6 @@
 namespace webapp_accessability.Models;
 
 public class Betalingsgegevens : ApplicationUser {
-    public string? IBAN {get; set;}
+    public string IBAN {get; set;}
     public int? Salaris {get; set;}
 }
