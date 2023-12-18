282c921dd8485cc23257876d7d872772567499e1
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-12-18 16:46:25 +0100

    Created a Bootstrap NavBar

--- webapp-accessability/ClientApp/src/components/Layout.jsx
+++ webapp-accessability/ClientApp/src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import { NavMenu } from './navbar/NavMenu';
+import NavMenu from './navbar/NavMenu';
 import '../stylesheets/Global.css'
 
 
