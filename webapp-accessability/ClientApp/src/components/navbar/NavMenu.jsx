b01ba6292d2024a2daeccb50c7f1d362681667b9
Ruben Bouwman <rubensb2001@gmail.com>
2023-12-21 13:45:33 +0100

    Update NavMenu.jsx
    Seperated the code by making components

--- webapp-accessability/ClientApp/src/components/navbar/NavMenu.jsx
+++ webapp-accessability/ClientApp/src/components/navbar/NavMenu.jsx
@@ -6,18 +6,30 @@ const NavMenu = () => {
     return (
         <nav className='navbar navbar-expand-lg navbar-dark' style={{ backgroundColor: 'rgb(29, 35, 79)', borderBottom: '0.4mm solid black', marginBottom: '15px', zIndex: '99' }}>
             <a className='title' href="#">Access-Ability</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className='nav-item' href="#">item1</a>
-                    <a className='nav-item' href="#">item2</a>
-                    <a className='nav-item' href="#">item3</a>
-                    <a className='nav-item' href="#">item4</a>
-                </div>
-            </div>
+            <Hamburger />
+            <Navitems />
         </nav>
+    );
+}
+
+const Hamburger = () => {
+    return (
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+    );
+}
+
+const Navitems = () => {
+    return (
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+                <a className='nav-item' href="#">item1</a>
+                <a className='nav-item' href="#">item2</a>
+                <a className='nav-item' href="#">item3</a>
+                <a className='nav-item' href="#">item4</a>
+            </div>
+        </div>
     );
 }
 
