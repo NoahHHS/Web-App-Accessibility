b8f456ba56ecbeffccdc4afb9cc1c9e19e15e7d9
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2023-12-22 14:49:00 +0100

    Made Profile Page

--- webapp-accessability/ClientApp/src/components/profiel/Profiel.jsx
+++ webapp-accessability/ClientApp/src/components/profiel/Profiel.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../../stylesheets/Profiel.css'
 
 export class Profiel extends Component {
     static displayName = Profiel.name;
@@ -6,7 +7,29 @@ export class Profiel extends Component {
   render() {
     return (
       <div>
+        <h1 className='pagetitle'>Mijn Gegevens</h1>
+        <section className='persoonsGegevens'>
+          <DataItem>Naam</DataItem>
+        </section>
+        <section className='Adres'>
+
+        </section>
+        <section className='medischeGegevens'>
+
+        </section>
+        <section className='Bedrijfsgegevens'>
+
+        </section>
       </div>
     );
   }
+}
+
+const DataItem = (prop) => {
+  return(
+    <div className='DataItem'>
+      <p className='DataItem-Name'>{prop}</p>
+      <input id='info-name' className="DataItem-Field" type="text" />
+    </div>
+  );
 }
