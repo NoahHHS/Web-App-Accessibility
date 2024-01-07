8e32fd8a147e7e8f0653f7561be48c206227686c
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2024-01-07 18:32:53 +0100

    Added more textfields and sections

--- webapp-accessability/ClientApp/src/components/profiel/Profiel.jsx
+++ webapp-accessability/ClientApp/src/components/profiel/Profiel.jsx
@@ -9,18 +9,22 @@ export class Profiel extends Component {
       <div>
         <h1 className='pagetitle'>Mijn Gegevens</h1>
         <section className='persoonsGegevens'>
+          <h2 className='subtitle'>Persoonlijke gegevens</h2>
           <DataItem value="Naam"/>
           <DataItem value="Achternaam"/>
           <DataItem value="Email"/>
         </section>
         <section className='Adres'>
-
+        <h2 className='subtitle'>Adres</h2>
+          <DataItem value="Straatnaam"/>
+          <DataItem value="Huisnummer"/>
+          <DataItem value="Postcode"/>
+          <DataItem value="Stad"/>
         </section>
         <section className='medischeGegevens'>
-
-        </section>
-        <section className='Bedrijfsgegevens'>
-
+        <h2 className='subtitle'>Medische gegevens</h2>
+          <DataItem value="Ziekte"/>
+          <DataItem value="Hulpmiddelen"/>
         </section>
       </div>
     );
