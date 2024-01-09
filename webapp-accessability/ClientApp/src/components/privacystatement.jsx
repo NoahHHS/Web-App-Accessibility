ec48d576dac9e7ca261db41545c4816e141fc3dc
MaikeruB <maikelbazuin@gmail.com>
2024-01-09 15:10:33 +0100

    privacystatment fixed

--- webapp-accessability/ClientApp/src/components/privacystatement.jsx
+++ webapp-accessability/ClientApp/src/components/privacystatement.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../stylesheets/privacystatement.css'
 
 export class Privacystatement extends Component {
     static displayName = Privacystatement.name;
