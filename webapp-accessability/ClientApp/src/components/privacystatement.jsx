cc23578f4d68f8f859a88acba6c5b9d99c617c4e
MaikeruB <maikelbazuin@gmail.com>
2024-01-09 16:13:06 +0100

    privacystatement fix 2

--- webapp-accessability/ClientApp/src/components/privacystatement.jsx
+++ webapp-accessability/ClientApp/src/components/privacystatement.jsx
@@ -11,7 +11,7 @@ export class Privacystatement extends Component {
         <section>
             <h2 class="subtitle">Persoonsgegevens die wij verwerken</h2>
             <p class="beschrijving">Accessibility verwerkt uw persoonsgegevens doordat u gebruik maakt van onze diensten en/of omdat u deze zelf aan ons verstrekt. Hieronder vindt u een overzicht van de persoonsgegevens die wij verwerken: </p>
-            <ul>
+            <ul class="Plist">
                 <li>Voor- en achternaam</li>
                 <li>Geslacht</li>
                 <li>Geboortedatum</li>
@@ -28,7 +28,7 @@ export class Privacystatement extends Component {
         <section>
             <h2 class="subtitle">Bijzondere en/ of gevoelige persoonsgegevens die wij verwerken </h2>
             <p class="beschrijving">Accessibility verwerkt de volgende bijzondere en/of gevoelige persoonsgegevens van u: </p>
-            <ul>
+            <ul class="Plist">
                 <li>gezondheid</li>
                 <li>gegevens van personen jonger dan 16 jaar Onze website en/of dienst heeft niet de intentie gegevens te verzamelen over websitebezoekers die jonger zijn dan16 jaar. Tenzij ze toestemming hebben van ouders of voogd. We kunnen echter niet controleren of een bezoeker ouder dan 16 is. Wij raden ouders dan ook aan betrokken te zijn bij de online activiteiten van hun kinderen, om zo te voorkomen dat er gegevens over kinderen verzameld worden zonder ouderlijke toestemming. Als u er van overtuigd bent dat wij zonder die toestemming persoonlijke gegevens hebben verzameld over een minderjarige, neem dan contact met ons op via accessibility@bartimeus.nl, dan verwijderen wij deze informatie.</li>
                 <li>Genetische gegevens</li>
@@ -41,7 +41,7 @@ export class Privacystatement extends Component {
         <section>
             <h2 class="subtitle">Met welk doel en op basis van welke grondslag wij persoonsgegevens verwerken </h2>
             <p class="beschrijving">Accessibility verwerkt uw persoonsgegevens voor de volgende doelen: </p>
-            <ul>
+            <ul class="Plist">
                 <li>Het afhandelen van uw betaling</li>
                 <li>Verzenden van onze nieuwsbrief en/of reclamefolder</li>
                 <li>U te kunnen bellen of e-mailen indien dit nodig is om onze dienstverlening uit te kunnen voeren</li>
