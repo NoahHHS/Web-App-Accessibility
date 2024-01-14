f59a8e6d58f42f8733fe5c7801f1f3c0c54a32d3
Ruben Bouwman <rubensb2001@gmail.com>
2024-01-14 17:26:15 +0100

    Readability verbeterd in privacystatement

--- webapp-accessability/ClientApp/src/components/privacystatement.jsx
+++ webapp-accessability/ClientApp/src/components/privacystatement.jsx
@@ -7,74 +7,76 @@ export class Privacystatement extends Component {
   render() {
     return (
       <div>
-        <h1 class="pagetitle">Privacystatement</h1>
-        <section>
-            <h2 class="subtitle">Persoonsgegevens die wij verwerken</h2>
-            <p class="beschrijving">Accessibility verwerkt uw persoonsgegevens doordat u gebruik maakt van onze diensten en/of omdat u deze zelf aan ons verstrekt. Hieronder vindt u een overzicht van de persoonsgegevens die wij verwerken: </p>
-            <ul class="Plist">
-                <li>Voor- en achternaam</li>
-                <li>Geslacht</li>
-                <li>Geboortedatum</li>
-                <li>Adresgegevens</li>
-                <li>Telefoonnummer</li>
-                <li>E-mailadres</li>
-                <li>IP-adres</li>
-                <li>Overige persoonsgegevens die u actief verstrekt bijvoorbeeld door een profiel op deze website aan te maken, in correspondentie en telefonisch</li>
-                <li>Locatiegegevens</li>
-                <li>Gegevens over uw activiteiten op onze website</li>
-                <li>Bankrekeningnummer</li>
-            </ul>
-        </section>
-        <section>
-            <h2 class="subtitle">Bijzondere en/ of gevoelige persoonsgegevens die wij verwerken </h2>
-            <p class="beschrijving">Accessibility verwerkt de volgende bijzondere en/of gevoelige persoonsgegevens van u: </p>
-            <ul class="Plist">
-                <li>gezondheid</li>
-                <li>gegevens van personen jonger dan 16 jaar Onze website en/of dienst heeft niet de intentie gegevens te verzamelen over websitebezoekers die jonger zijn dan16 jaar. Tenzij ze toestemming hebben van ouders of voogd. We kunnen echter niet controleren of een bezoeker ouder dan 16 is. Wij raden ouders dan ook aan betrokken te zijn bij de online activiteiten van hun kinderen, om zo te voorkomen dat er gegevens over kinderen verzameld worden zonder ouderlijke toestemming. Als u er van overtuigd bent dat wij zonder die toestemming persoonlijke gegevens hebben verzameld over een minderjarige, neem dan contact met ons op via accessibility@bartimeus.nl, dan verwijderen wij deze informatie.</li>
-                <li>Genetische gegevens</li>
-            </ul>      
-        </section>
-        <section>
-            <h2 class="subtitle">Bijzondere en/of gevoelige persoonsgegevens die wij verwerken </h2>
-            <p class="beschrijving">Onze website en/of dienst heeft niet de intentie gegevens te verzamelen over websitebezoekers die jonger zijn dan 16 jaar. Tenzij ze toestemming hebben van ouders of voogd. We kunnen echter niet controleren of een bezoeker ouder dan 16 is. Wij raden ouders dan ook aan betrokken te zijn bij de online activiteiten van hun kinderen, om zo te voorkomen dat er gegevens over kinderen verzameld worden zonder ouderlijke toestemming. Als u er van overtuigd bent dat wij zonder die toestemming persoonlijke gegevens hebben verzameld over een minderjarige, neem dan contact met ons op via accessibility@bartimeus.nl, dan verwijderen wij deze informatie. </p>
-        </section>
-        <section>
-            <h2 class="subtitle">Met welk doel en op basis van welke grondslag wij persoonsgegevens verwerken </h2>
-            <p class="beschrijving">Accessibility verwerkt uw persoonsgegevens voor de volgende doelen: </p>
-            <ul class="Plist">
-                <li>Het afhandelen van uw betaling</li>
-                <li>Verzenden van onze nieuwsbrief en/of reclamefolder</li>
-                <li>U te kunnen bellen of e-mailen indien dit nodig is om onze dienstverlening uit te kunnen voeren</li>
-                <li>U te informeren over wijzigingen van onze diensten en producten</li>
-                <li>U de mogelijkheid te bieden een account aan te maken</li>
-            </ul>
-        </section>
-        <section>
-            <h2 class="subtitle">Geautomatiseerde besluitvorming </h2>
-            <p class="beschrijving">Accessibility neemt #responsibility op basis van geautomatiseerde verwerkingen besluiten over zaken die (aanzienlijke) gevolgen kunnen hebben voor personen.</p>
-            <p class="beschrijving">Het gaat hier om besluiten die worden genomen door computerprogramma's of -systemen, zonder dat daar een mens (bijvoorbeeld een medewerker van Accessibility) tussen zit. Accessibility gebruikt de volgende computerprogramma's of -systemen: #use_explanation</p>
-        </section>
-        <section>
-            <h2 class="subtitle">Hoe lang we persoonsgegevens bewaren </h2>
-            <p class="beschrijving">Accessibility bewaart uw persoonsgegevens niet langer dan strikt nodig is om de doelen te realiseren waarvoor uw gegevens worden verzameld. Wij hanteren de volgende bewaartermijnen voor de volgende (categorieën) van persoonsgegevens: #retention_period.  Deze gegevens bewaren we voor een onbeperkte tijd totdat een gebruiker dit zelf vraagt of het account voor een jaar na een remindermail niet is gebruikt. </p>
-        </section>
-        <section>
-            <h2 class="subtitle">Delen van persoonsgegevens met derden </h2>
-            <p class="beschrijving">Accessibility verstrekt uitsluitend aan derden en alleen als dit nodig is voor de uitvoering van onze overeenkomst met u of om te voldoen aan een wettelijke verplichting. In ons geval zullen wij gegevens aan derden verstrekken die hier relevantie tot hebben, bijv. Bedrijven van onderzoeken waarop jij reageert.</p>
-        </section>
-        <section>
-            <h2 class="subtitle">Cookies, of vergelijkbare technieken, die wij gebruiken </h2>
-            <p class="beschrijving">Accessibility gebruikt alleen technische en functionele cookies. En analytische cookies die geen inbreuk maken op uw privacy. Een cookie is een klein tekstbestand dat bij het eerste bezoek aan deze website wordt opgeslagen op uw computer, tablet of smartphone. De cookies die wij gebruiken zijn noodzakelijk voor de technische werking van de website en uw gebruiksgemak. Ze zorgen ervoor dat de website naar behoren werkt en onthouden bijvoorbeeld uw voorkeursinstellingen. Ook kunnen wij hiermee onze website optimaliseren. U kunt zich afmelden voor cookies door uw internetbrowser zo in te stellen dat deze geen cookies meer opslaat. Daarnaast kunt u ook alle informatie die eerder is opgeslagen via de instellingen van uw browser verwijderen.</p>
-        </section>
-        <section>
-            <h2 class="subtitle">Gegevens inzien, aanpassen of verwijderen </h2>
-            <p class="beschrijving">U heeft het recht om uw persoonsgegevens in te zien, te corrigeren of te verwijderen. Dit kunt u zelf doen via de persoonlijke instellingen van uw account. Daarnaast heeft u het recht om uw eventuele toestemming voor de gegevensverwerking in te trekken of bezwaar te maken tegen de verwerking van uw persoonsgegevens door ons bedrijf en heeft u het recht op gegevensoverdraagbaarheid. Dat betekent dat u bij ons een verzoek kunt indienen om de persoonsgegevens die wij van u beschikken in een computerbestand naar u of een ander, door u genoemde organisatie, te sturen. Wilt u gebruik maken van uw recht op bezwaar en/of recht op gegevensoverdraagbaarheid of heeft u andere vragen/opmerkingen over de gegevensverwerking, stuur dan een gespecificeerd verzoek naar accessibility@bartimeus.nl. Om er zeker van te zijn dat het verzoek tot inzage door u is gedaan, vragen wij u een kopie van uw identiteitsbewijs bij het verzoek mee te sturen. Maak in deze kopie uw pasfoto, MRZ (machine readable zone, de strook met nummers onderaan het paspoort), paspoortnummer en Burgerservicenummer (BSN) zwart. Dit ter bescherming van uw privacy. Accessibility zal zo snel mogelijk, maar in ieder geval binnen vier weken, op uw verzoek reageren. Accessibility wil u er tevens op wijzen dat u de mogelijkheid hebt om een klacht in te dienen bij de nationale toezichthouder, de Autoriteit Persoonsgegevens.</p>
-            <p class="beschrijving">Dat kan via de volgende link <a href="https://autoriteitpersoonsgegevens.nl/nl/contact-met-de-autoriteit-persoonsgegevens/tip-ons" alt="link naar autoriteitpersoonsgegevens">autoriteitpersoonsgegevens</a> </p>
-        </section>
-        <section>
-            <h2  class="subtitle">Hoe wij persoonsgegevens beveiligen </h2>
-            <p class="bechrijving">Accessibility neemt de bescherming van uw gegevens serieus en neemt passende maatregelen om misbruik, verlies, onbevoegde toegang, ongewenste openbaarmaking en ongeoorloofde wijziging tegen te gaan. Als u de indruk heeft dat uw gegevens niet goed beveiligd zijn of er aanwijzingen zijn van misbruik, neem dan contact op met onze klantenservice of via accessibility@bartimeus.nl </p>
-        </section>
+        <div className='Privacy-Content'>
+            <h1 class="pagetitle">Privacystatement</h1>
+            <section>
+                <h2 class="subtitle">Persoonsgegevens die wij verwerken</h2>
+                <p class="beschrijving">Accessibility verwerkt uw persoonsgegevens doordat u gebruik maakt van onze diensten en/of omdat u deze zelf aan ons verstrekt.< br/>Hieronder vindt u een overzicht van de persoonsgegevens die wij verwerken: </p>
+                <ul class="Plist">
+                    <li>Voor- en achternaam</li>
+                    <li>Geslacht</li>
+                    <li>Geboortedatum</li>
+                    <li>Adresgegevens</li>
+                    <li>Telefoonnummer</li>
+                    <li>E-mailadres</li>
+                    <li>IP-adres</li>
+                    <li>Overige persoonsgegevens die u actief verstrekt bijvoorbeeld door een profiel op deze website aan te maken,< br/>in correspondentie en telefonisch</li>
+                    <li>Locatiegegevens</li>
+                    <li>Gegevens over uw activiteiten op onze website</li>
+                    <li>Bankrekeningnummer</li>
+                </ul>
+            </section>
+            <section>
+                <h2 class="subtitle">Bijzondere en/ of gevoelige persoonsgegevens die wij verwerken </h2>
+                <p class="beschrijving">Accessibility verwerkt de volgende bijzondere en/of gevoelige persoonsgegevens van u: </p>
+                <ul class="Plist">
+                    <li>gezondheid</li>
+                    <li>gegevens van personen jonger dan 16 jaar Onze website en/of dienst heeft niet de intentie gegevens te verzamelen over websitebezoekers die jonger zijn dan 16 jaar. Tenzij ze toestemming hebben van ouders of voogd. We kunnen echter niet controleren of een bezoeker ouder dan 16 is. Wij raden ouders dan ook aan betrokken te zijn bij de online activiteiten van hun kinderen, om zo te voorkomen dat er gegevens over kinderen verzameld worden zonder ouderlijke toestemming. Als u er van overtuigd bent dat wij zonder die toestemming persoonlijke gegevens hebben verzameld over een minderjarige, neem dan contact met ons op via accessibility@bartimeus.nl, dan verwijderen wij deze informatie.</li>
+                    <li>Genetische gegevens</li>
+                </ul>      
+            </section>
+            <section>
+                <h2 class="subtitle">Bijzondere en/of gevoelige persoonsgegevens die wij verwerken </h2>
+                <p class="beschrijving">Onze website en/of dienst heeft niet de intentie gegevens te verzamelen over websitebezoekers die jonger zijn dan 16 jaar. Tenzij ze toestemming hebben van ouders of voogd. We kunnen echter niet controleren of een bezoeker ouder dan 16 is.< br/>< br/>Wij raden ouders dan ook aan betrokken te zijn bij de online activiteiten van hun kinderen, om zo te voorkomen dat er gegevens over kinderen verzameld worden zonder ouderlijke toestemming. Als u er van overtuigd bent dat wij zonder die toestemming persoonlijke gegevens hebben verzameld over een minderjarige, neem dan contact met ons op via accessibility@bartimeus.nl, dan verwijderen wij deze informatie. </p>
+            </section>
+            <section>
+                <h2 class="subtitle">Met welk doel en op basis van welke grondslag wij persoonsgegevens verwerken </h2>
+                <p class="beschrijving">Accessibility verwerkt uw persoonsgegevens voor de volgende doelen: </p>
+                <ul class="Plist">
+                    <li>Het afhandelen van uw betaling</li>
+                    <li>Verzenden van onze nieuwsbrief en/of reclamefolder</li>
+                    <li>U te kunnen bellen of e-mailen indien dit nodig is om onze dienstverlening uit te kunnen voeren</li>
+                    <li>U te informeren over wijzigingen van onze diensten en producten</li>
+                    <li>U de mogelijkheid te bieden een account aan te maken</li>
+                </ul>
+            </section>
+            <section>
+                <h2 class="subtitle">Geautomatiseerde besluitvorming </h2>
+                <p class="beschrijving">Accessibility neemt #responsibility op basis van geautomatiseerde verwerkingen besluiten over zaken die (aanzienlijke) gevolgen kunnen hebben voor personen.</p>
+                <p class="beschrijving">Het gaat hier om besluiten die worden genomen door computerprogramma's of -systemen, zonder dat daar een mens (bijvoorbeeld een medewerker van Accessibility) tussen zit. Accessibility gebruikt de volgende computerprogramma's of -systemen: #use_explanation</p>
+            </section>
+            <section>
+                <h2 class="subtitle">Hoe lang we persoonsgegevens bewaren </h2>
+                <p class="beschrijving">Accessibility bewaart uw persoonsgegevens niet langer dan strikt nodig is om de doelen te realiseren waarvoor uw gegevens worden verzameld. Wij hanteren de volgende bewaartermijnen voor de volgende (categorieën) van persoonsgegevens: #retention_period.  Deze gegevens bewaren we voor een onbeperkte tijd totdat een gebruiker dit zelf vraagt of het account voor een jaar na een remindermail niet is gebruikt. </p>
+            </section>
+            <section>
+                <h2 class="subtitle">Delen van persoonsgegevens met derden </h2>
+                <p class="beschrijving">Accessibility verstrekt uitsluitend aan derden en alleen als dit nodig is voor de uitvoering van onze overeenkomst met u of om te voldoen aan een wettelijke verplichting. In ons geval zullen wij gegevens aan derden verstrekken die hier relevantie tot hebben, bijv. Bedrijven van onderzoeken waarop jij reageert.</p>
+            </section>
+            <section>
+                <h2 class="subtitle">Cookies, of vergelijkbare technieken, die wij gebruiken </h2>
+                <p class="beschrijving">Accessibility gebruikt alleen technische en functionele cookies. En analytische cookies die geen inbreuk maken op uw privacy. Een cookie is een klein tekstbestand dat bij het eerste bezoek aan deze website wordt opgeslagen op uw computer, tablet of smartphone.< br/>< br/>De cookies die wij gebruiken zijn noodzakelijk voor de technische werking van de website en uw gebruiksgemak. Ze zorgen ervoor dat de website naar behoren werkt en onthouden bijvoorbeeld uw voorkeursinstellingen. Ook kunnen wij hiermee onze website optimaliseren. U kunt zich afmelden voor cookies door uw internetbrowser zo in te stellen dat deze geen cookies meer opslaat. Daarnaast kunt u ook alle informatie die eerder is opgeslagen via de instellingen van uw browser verwijderen.</p>
+            </section>
+            <section>
+                <h2 class="subtitle">Gegevens inzien, aanpassen of verwijderen </h2>
+                <p class="beschrijving">U heeft het recht om uw persoonsgegevens in te zien, te corrigeren of te verwijderen. Dit kunt u zelf doen via de persoonlijke instellingen van uw account. Daarnaast heeft u het recht om uw eventuele toestemming voor de gegevensverwerking in te trekken of bezwaar te maken tegen de verwerking van uw persoonsgegevens door ons bedrijf en heeft u het recht op gegevensoverdraagbaarheid. Dat betekent dat u bij ons een verzoek kunt indienen om de persoonsgegevens die wij van u beschikken in een computerbestand naar u of een ander, door u genoemde organisatie, te sturen.< br/>< br/>Wilt u gebruik maken van uw recht op bezwaar en/of recht op gegevensoverdraagbaarheid of heeft u andere vragen/opmerkingen over de gegevensverwerking, stuur dan een gespecificeerd verzoek naar accessibility@bartimeus.nl. Om er zeker van te zijn dat het verzoek tot inzage door u is gedaan, vragen wij u een kopie van uw identiteitsbewijs bij het verzoek mee te sturen.< br/>< br/>Maak in deze kopie uw pasfoto, MRZ (machine readable zone, de strook met nummers onderaan het paspoort), paspoortnummer en Burgerservicenummer (BSN) zwart. Dit ter bescherming van uw privacy. Accessibility zal zo snel mogelijk, maar in ieder geval binnen vier weken, op uw verzoek reageren.< br/>< br/>Accessibility wil u er tevens op wijzen dat u de mogelijkheid hebt om een klacht in te dienen bij de nationale toezichthouder, de Autoriteit Persoonsgegevens.</p>
+                <p class="beschrijving">Dat kan via de volgende link <a href="https://autoriteitpersoonsgegevens.nl/nl/contact-met-de-autoriteit-persoonsgegevens/tip-ons" alt="link naar autoriteitpersoonsgegevens">autoriteitpersoonsgegevens</a> </p>
+            </section>
+            <section>
+                <h2 class="subtitle">Hoe wij persoonsgegevens beveiligen </h2>
+                <p class="beschrijving">Accessibility neemt de bescherming van uw gegevens serieus en neemt passende maatregelen om misbruik, verlies, onbevoegde toegang, ongewenste openbaarmaking en ongeoorloofde wijziging tegen te gaan.< br/>< br/>Als u de indruk heeft dat uw gegevens niet goed beveiligd zijn of er aanwijzingen zijn van misbruik, neem dan contact op met onze klantenservice of via accessibility@bartimeus.nl </p>
+            </section>
+        </div>
       </div>
     );
   }
