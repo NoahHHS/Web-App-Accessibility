9e623f49138268f4b48ad238b09ccd1e82481129
Ruben Bouwman <rubensb2001@gmail.com>
2024-01-14 15:30:33 +0100

    fixed bug

--- webapp-accessability/ClientApp/src/components/profiel/Profiel.jsx
+++ webapp-accessability/ClientApp/src/components/profiel/Profiel.jsx
@@ -8,20 +8,20 @@ export class Profiel extends Component {
     return (
       <div>
         <h1 className='pagetitle'>Mijn Gegevens</h1>
-        <section className='persoonsGegevens'>
+        <section className='profiel-section'>
           <h2 className='subtitle'>Persoonlijke gegevens</h2>
           <DataItem value="Naam"/>
           <DataItem value="Achternaam"/>
           <DataItem value="Email"/>
         </section>
-        <section className='Adres'>
+        <section className='profiel-section'>
         <h2 className='subtitle'>Adres</h2>
           <DataItem value="Straatnaam"/>
           <DataItem value="Huisnummer"/>
           <DataItem value="Postcode"/>
           <DataItem value="Stad"/>
         </section>
-        <section className='medischeGegevens'>
+        <section className='profiel-section'>
         <h2 className='subtitle'>Medische gegevens</h2>
           <DataItem value="Ziekte"/>
           <DataItem value="Hulpmiddelen"/>
