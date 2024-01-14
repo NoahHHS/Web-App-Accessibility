fcb2841c2315f4391543d5bf57483caf12633b10
Ruben Bouwman <rubensb2001@gmail.com>
2024-01-14 18:44:27 +0100

    Updated Profiel Page

--- webapp-accessability/ClientApp/src/components/profiel/Profiel.jsx
+++ webapp-accessability/ClientApp/src/components/profiel/Profiel.jsx
@@ -14,6 +14,7 @@ export class Profiel extends Component {
           <DataItem value="Achternaam"/>
           <DataItem value="Email"/>
         </section>
+        <SaveButton/>
         <section className='profiel-section'>
         <h2 className='subtitle'>Adres</h2>
           <DataItem value="Straatnaam"/>
@@ -21,14 +22,25 @@ export class Profiel extends Component {
           <DataItem value="Postcode"/>
           <DataItem value="Stad"/>
         </section>
+        <SaveButton/>
         <section className='profiel-section'>
         <h2 className='subtitle'>Medische gegevens</h2>
           <DataItem value="Ziekte"/>
           <DataItem value="Hulpmiddelen"/>
         </section>
+        <SaveButton/>
       </div>
     );
   }
+}
+
+const SaveButton = () => {
+    return(
+      <div className='SaveButton-Content'>
+        <button className='SaveButton' title='Sla veranderingen op'><strong>Save</strong></button>
+        <p className='SaveButton-Warning'>Sla je veranderingen op!<br/>anders worden ze ongedaan gemaakt</p>
+      </div>
+    );
 }
 
 const DataItem = (prop) => {
