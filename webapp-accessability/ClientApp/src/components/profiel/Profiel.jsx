e6a97232b1ace395971c808f35441357bfa87150
Ruben Bouwman <129963248+rubenbouwman@users.noreply.github.com>
2024-01-15 13:44:23 +0100

    Update Profiel.jsx

--- webapp-accessability/ClientApp/src/components/profiel/Profiel.jsx
+++ webapp-accessability/ClientApp/src/components/profiel/Profiel.jsx
@@ -10,23 +10,23 @@ export class Profiel extends Component {
         <h1 className='pagetitle'>Mijn Gegevens</h1>
         <section className='profiel-section'>
           <h2 className='subtitle'>Persoonlijke gegevens</h2>
-          <DataItem value="Naam"/>
-          <DataItem value="Achternaam"/>
-          <DataItem value="Email"/>
+          <DataItem value="Naam" aria-label='Naam invoerveld, voer hier je naam in'/>
+          <DataItem value="Achternaam" aria-label='Achternaam invoerveld, voer hier je achternaam in'/>
+          <DataItem value="Email" aria-label='Email invoerveld, voer hier je email in'/>
         </section>
         <SaveButton/>
         <section className='profiel-section'>
         <h2 className='subtitle'>Adres</h2>
-          <DataItem value="Straatnaam"/>
-          <DataItem value="Huisnummer"/>
-          <DataItem value="Postcode"/>
-          <DataItem value="Stad"/>
+          <DataItem value="Straatnaam" aria-label='Straatnaam invoerveld, voer hier je straatnaam in'/>
+          <DataItem value="Huisnummer" aria-label='huisnummer invoerveld, voer hier de bijhorende huisnummer in'/>
+          <DataItem value="Postcode" aria-label='Postcode invoerveld, voer hier je postcode in, geen spaties'/>
+          <DataItem value="Stad" aria-label='Stad invoerveld, voer hier in, in welke stad je woont'/>
         </section>
         <SaveButton/>
         <section className='profiel-section'>
         <h2 className='subtitle'>Medische gegevens</h2>
-          <DataItem value="Ziekte"/>
-          <DataItem value="Hulpmiddelen"/>
+          <DataItem value="Ziekte" aria-label='Ziekte invoerveld, voer hier je Ziektes in, onderschijd de ziektes met een komma en een spatie'/>
+          <DataItem value="Hulpmiddelen" aria-label='Hulpmiddelen invoerveld, voer de hulpmiddelen in die je gebruikt, onderschijd de middelen met een komma en spatie'/>
         </section>
         <SaveButton/>
       </div>
