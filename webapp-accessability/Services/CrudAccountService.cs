904e283feaf07c6b6b0d12ec1d2ae0bab9510a7d
Ruben Bouwman <rubensb2001@gmail.com>
2024-01-17 16:09:23 +0100

    Update CrudAccountService.cs
    Implemented interface

--- webapp-accessability/Services/CrudAccountService.cs
+++ webapp-accessability/Services/CrudAccountService.cs
@@ -1,3 +1,24 @@
-public class CrudAccountService : ICrudService{
-    
+using webapp_accessability.Models;
+
+public class CrudAccountService : ICrudService<ApplicationUser>
+{
+    public void Aanmaken(ApplicationUser obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Lezen(string Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(ApplicationUser obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Verwijderen(string Id)
+    {
+        throw new NotImplementedException();
+    }
 }
