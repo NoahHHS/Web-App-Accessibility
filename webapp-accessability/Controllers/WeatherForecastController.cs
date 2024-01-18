0acea8c62a3cb925c5578da413fe8d317463e4c8
NoahHHS <101333810+NoahHHS@users.noreply.github.com>
2024-01-18 22:21:54 +0100

    swagger toegevoegd

--- webapp-accessability/Controllers/WeatherForecastController.cs
+++ webapp-accessability/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace webapp_accessability.Controllers;
 
-[Authorize]
+// [Authorize]
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
