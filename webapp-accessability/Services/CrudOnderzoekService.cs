76e67a05756172aaada8bb790b30605f2d2c8515
Ruben Bouwman <rubensb2001@gmail.com>
2024-01-18 13:37:10 +0100

    Create CrudOnderzoekService.cs

---
+++ webapp-accessability/Services/CrudOnderzoekService.cs
@@ -0,0 +1,24 @@
+using webapp_accessability.Models;
+
+public class CrudOnderzoekService : ICrudService<Onderzoek>
+{
+    public void Create(Onderzoek onderzoek)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Onderzoek Read(string Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(string Id, Onderzoek obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(string Id)
+    {
+        throw new NotImplementedException();
+    }
+}
