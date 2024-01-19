e559b4c10b4290f822cbe419b28231e23c0284e7
Msa-786 <94054037+Msa-786@users.noreply.github.com>
2024-01-19 21:09:11 +0100

    onderzoekscontroller gemaakt
    onderzoekscontroller aangemaakt met een paar methods

---
+++ webapp-accessability/Controllers/OnderzoeksController.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using webapp_accessability.Data;
+using webapp_accessability.Models;
+
+[Route("api/onderzoek")]
+[ApiController]
+public class OnderzoeksController : ControllerBase
+{
+    private ApplicationDbContext context;
+    private readonly CrudOnderzoekService _onderzoekService;
+     public OnderzoeksController(CrudOnderzoekService onderzoekService)
+    {
+        _onderzoekService = onderzoekService;
+    }
+    
+    [Route("api/onderzoek")]
+    public IActionResult GetNaamEnBeschrijving()
+    {
+
+        var onderzoeken = _onderzoekService.ReadAll();
+
+        var result = onderzoeken.Select(o => new
+        {
+            Naam = o.Naam,
+            Beschrijving = o.Omschrijving
+
+        }).ToList();
+
+        return Ok(result);
+    }
+    
+    [Route("api/onderzoek")]
+    public IActionResult GetNaam(string Id)
+    {
+        var Onderzoek = context.Onderzoeken.FirstOrDefault(O => O.Id.ToString() == Id);
+        var result = context.Onderzoeken.Select(O => new 
+        {
+            Naam = O.Naam
+        }).ToList();
+
+        return Ok(result);
+
+    }
+    [HttpPost]
+    [Route("api/onderzoek")]
+    public IActionResult Deelnemen([FromBody]Deelname deelname)
+    {
+        if (ModelState.IsValid)
+        {
+            // Maak een nieuw Onderzoek-object met de ontvangen gegevens
+            var nieuweDeelname = new Deelname
+            {
+                ApplicationUserId = deelname.ApplicationUserId,
+                OnderzoekId = deelname.OnderzoekId  
+                // Vul andere velden in zoals gewenst
+            };
+            _onderzoekService.AddDeelnemer(nieuweDeelname);
+}
+    return Ok("Deelname succesvol");
+    
+    }
+    }
+
