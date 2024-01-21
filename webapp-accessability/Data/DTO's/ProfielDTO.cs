de8366264d135eaa2dcb6962a773f457f7e80e57
Ruben Bouwman <rubensb2001@gmail.com>
2024-01-21 18:27:29 +0100

    Changed name to ProfielDTO and removed item

--- webapp-accessability/Data/DTO's/ProfielDTO.cs
+++ webapp-accessability/Data/DTO's/ProfielDTO.cs
@@ -1,10 +1,9 @@
 namespace webapp_accessability.Models;
 // RegistrationModel
-public class AccountDTO
+public class ProfielDTO
 {
     public string? Naam { get; set; }
     public string? Email { get; set; }
-    public string? BedrijfsNaam { get; set; }
     public DateTime? Beschikbaarheid { get; set; }
     public string Straat { get; set; }
     public int HuisNr { get; set; }
