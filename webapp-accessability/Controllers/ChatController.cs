8bb9631eb8f5ec28c7a076940fdf620a0efcc625
Ruben Bouwman <rubensb2001@gmail.com>
2024-01-24 23:40:22 +0100

    Update ChatController.cs

--- webapp-accessability/Controllers/ChatController.cs
+++ webapp-accessability/Controllers/ChatController.cs
@@ -1,25 +1,25 @@
-using Microsoft.AspNetCore.Mvc;
-using webapp_accessability.Data;
-using webapp_accessability.Models;
+// using Microsoft.AspNetCore.Mvc;
+// using webapp_accessability.Data;
+// using webapp_accessability.Models;
 
-[ApiController]
-[Route("[controller]")]
-public class ChatController : ControllerBase {
+// [ApiController]
+// [Route("[controller]")]
+// public class ChatController : ControllerBase {
 
-    private ApplicationDbContext context;
-    public ChatController(ApplicationDbContext _c) {
-        this.context = _c;
-    }
+//     private ApplicationDbContext context;
+//     public ChatController(ApplicationDbContext _c) {
+//         this.context = _c;
+//     }
 
-    [HttpGet] // geeft message terug
-    public IActionResult Get(string Id) {
-        int IId = Int32.Parse(Id);
+//     [HttpGet] // geeft message terug
+//     public IActionResult Get(string Id) {
+//         int IId = Int32.Parse(Id);
 
-        return Ok(context.Chats.Where(ch => ch._to == IId).FirstOrDefault().message);
-    }
+//         return Ok(context.Chats.Where(ch => ch._to == IId).FirstOrDefault().message);
+//     }
 
-    // [HttpPost]  stuurt bericht door naar DB
-    // public IActionResult PostMessage(Chat chat) {
+//     // [HttpPost]  stuurt bericht door naar DB
+//     // public IActionResult PostMessage(Chat chat) {
         
-    // }
-}
+//     // }
+// }
