608bde6d2243f693669f3f8387ae67094b778076
MaikeruB <maikelbazuin@gmail.com>
2024-01-24 19:44:04 +0100

    database update

--- webapp-accessability/Migrations/20240124184239_AddRelationToOnderzoek.Designer.cs
+++ webapp-accessability/Migrations/20240124184239_AddRelationToOnderzoek.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using webapp_accessability.Data;
 
@@ -10,9 +11,11 @@ using webapp_accessability.Data;
 namespace webapp_accessability.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240124184239_AddRelationToOnderzoek")]
+    partial class AddRelationToOnderzoek
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.12");
---
+++ webapp-accessability/Migrations/20240124184239_AddRelationToOnderzoek.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapp_accessability.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationToOnderzoek : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BedrijfId",
+                table: "Onderzoeken",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Achternaam",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Toevoeging",
+                table: "Adressen",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Onderzoeken_BedrijfId",
+                table: "Onderzoeken",
+                column: "BedrijfId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Onderzoeken_AspNetUsers_BedrijfId",
+                table: "Onderzoeken",
+                column: "BedrijfId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Onderzoeken_AspNetUsers_BedrijfId",
+                table: "Onderzoeken");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Onderzoeken_BedrijfId",
+                table: "Onderzoeken");
+
+            migrationBuilder.DropColumn(
+                name: "BedrijfId",
+                table: "Onderzoeken");
+
+            migrationBuilder.DropColumn(
+                name: "Achternaam",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Toevoeging",
+                table: "Adressen",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
