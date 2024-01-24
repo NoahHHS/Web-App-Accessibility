c9e300980dca67bb19e5e67007b3150d0099eaf0
MaikeruB <maikelbazuin@gmail.com>
2024-01-24 22:58:03 +0100

    controller update with dto

--- webapp-accessability/Controllers/BedrijfsController.cs
+++ webapp-accessability/Controllers/BedrijfsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using webapp_accessability.Models;
 
 [ApiController]
 [Route("[controller]")]
+[Authorize(Roles = "Bedrijf")]
 public class BedrijfsController : ControllerBase
 {
     private readonly ApplicationDbContext _context;
@@ -31,8 +33,47 @@ public class BedrijfsController : ControllerBase
 
     [HttpPost]
     [Route("CreateOnderzoek")]
-    public async Task<ActionResult<Onderzoek>> CreateOnderzoek(Onderzoek onderzoek)
+    public async Task<ActionResult<Onderzoek>> CreateOnderzoek(OnderzoekCreatieDTO onderzoekDTO)
     {
+        // Validate the model state
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        // Create OnderzoekLink entity
+        var link = new OnderzoekLink
+        {
+            Link = onderzoekDTO.Link?.Link // Ensure Link is not null before accessing Link property
+        };
+
+        // Create Adres entity
+        var adres = new Adres
+        {
+            Straat = onderzoekDTO.Locatie?.Adres?.Straat,
+            HuisNr = onderzoekDTO.Locatie?.Adres?.HuisNr ?? 0,
+            Toevoeging = onderzoekDTO.Locatie?.Adres?.Toevoeging,
+            Postcode = onderzoekDTO.Locatie?.Adres?.Postcode
+        };
+
+        // Create OnderzoekLocatie entity
+        var locatie = new OnderzoekLocatie
+        {
+            Adres = adres
+        };
+
+        // Create Onderzoek entity
+        var onderzoek = new Onderzoek
+        {
+            Naam = onderzoekDTO.Naam,
+            Omschrijving = onderzoekDTO.Omschrijving,
+            StartDatum = onderzoekDTO.StartDatum,
+            Link = link,
+            Locatie = locatie
+            // Add other properties as needed
+        };
+
+        // Add entities to context and save changes
         _context.Onderzoeken.Add(onderzoek);
         await _context.SaveChangesAsync();
 
---
+++ webapp-accessability/Data/DTO's/OnderzoekCreatieDTO.cs
@@ -0,0 +1,46 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace webapp_accessability.Models
+{
+    public class OnderzoekCreatieDTO
+    {
+        [Required(ErrorMessage = "The naam field is required.")]
+        public string Naam { get; set; }
+
+        [Required(ErrorMessage = "The beschrijving field is required.")]
+        public string Omschrijving { get; set; }
+
+        [Required(ErrorMessage = "The startDatum field is required.")]
+        public DateTime StartDatum { get; set; }
+
+        public OnderzoekLinkDTO Link { get; set; } // Adjust if necessary
+
+        public OnderzoekLocatieDTO Locatie { get; set; } // Adjust if necessary
+    }
+
+    public class OnderzoekLinkDTO
+    {
+        [Required(ErrorMessage = "The link field is required.")]
+        public string Link { get; set; }
+    }
+
+    public class OnderzoekLocatieDTO
+    {
+        [Required(ErrorMessage = "The adres field is required.")]
+        public AdresDTO Adres { get; set; }
+    }
+
+    public class AdresDTO
+    {
+        [Required(ErrorMessage = "The straat field is required.")]
+        public string Straat { get; set; }
+
+        [Required(ErrorMessage = "The huisNr field is required.")]
+        public int HuisNr { get; set; }
+
+        public string Toevoeging { get; set; }
+
+        [Required(ErrorMessage = "The postcode field is required.")]
+        public string Postcode { get; set; }
+    }
+}
